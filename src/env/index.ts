094259ac920c9594bc63415211ddbee1643f8bcf
Fernando Santos <fernandosantoshs@gmail.com>
2024-11-11 10:44:54 -0300

    Adds .env.example, env/index file, eslintignore, npmrc and prisma ORM

---
+++ src/env/index.ts
@@ -0,0 +1,23 @@
+import { config } from 'dotenv';
+import { z } from 'zod';
+
+if (process.env.NODE_ENV == 'test') {
+  config({ path: 'env.test' });
+} else {
+  config();
+}
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['dev', 'production', 'test']).default('dev'),
+  PORT: z.coerce.number().default(3333),
+});
+
+const envParse = envSchema.safeParse(process.env);
+
+if (envParse.success == false) {
+  console.error('Invalid enviroment variables', envParse.error.format());
+
+  throw new Error('Invalid enviroment variables');
+}
+
+export const env = envParse.data;
