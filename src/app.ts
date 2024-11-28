57c41bbd95c80082028bd97d2d9be86490fee59f
Fernando Santos <fernandosantoshs@gmail.com>
2024-11-28 12:03:16 -0300

    Adds prisma client, schema and create user migration; Adds bitnami docker image for postgres database

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,8 @@
+import { PrismaClient } from '@prisma/client';
 import { fastify } from 'fastify';
 
 export const app = fastify();
+
+const prisma = new PrismaClient();
+
+prisma.user.create({ data: { name: 'DevUser', email: 'devemail@dev.com' } });
