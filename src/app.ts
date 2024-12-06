0aa74816efcf813d0dc7b1dbff385195b039838b
Fernando Santos <fernandosantoshs@gmail.com>
2024-12-06 10:46:13 -0300

    Create tables relations; Adds password hash and created_at at user model

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,3 @@ import { PrismaClient } from '@prisma/client';
 import { fastify } from 'fastify';
 
 export const app = fastify();
-
-const prisma = new PrismaClient();
-
-prisma.user.create({ data: { name: 'DevUser', email: 'devemail@dev.com' } });
