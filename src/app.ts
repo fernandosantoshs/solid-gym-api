59731d553b046c5f5c124472005ba5d29d4a0a38
Fernando Santos <fernandosantoshs@gmail.com>
2024-12-19 09:48:23 -0300

    Adds first route(post user); Adds first plugin appRoutes

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
-import { PrismaClient } from '@prisma/client';
 import { fastify } from 'fastify';
+import { appRoutes } from './http/routes';
 
 export const app = fastify();
+
+app.register(appRoutes);
