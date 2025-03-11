a43fdd6abc8f10f1a6486c78409360c9992532c5
Fernando Santos <fernandosantoshs@gmail.com>
2025-03-11 12:01:13 -0300

    Adds create a new user test

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,26 +1,32 @@
-// Fazer teste de hash de password
-
 import { compare } from 'bcrypt';
 import { RegisterUseCase } from './register';
 import { describe, expect, it } from 'vitest';
+import { InMemoryUsersRepository } from '../repositories/in-memory/in-memory-users-repository';
 
 describe('Register use case', () => {
-  it('should hash the password', async () => {
-    const usersRepository = {
-      async findByEmail(email: string) {
-        return null;
-      },
+  it('should create a new user', async () => {
+    const usersRepository = new InMemoryUsersRepository();
+    const registerUseCase = new RegisterUseCase(usersRepository);
 
-      async create(data: any) {
-        return {
-          id: 'id-1',
-          name: data.name,
-          email: data.email,
-          password_hash: data.password_hash,
-          created_at: new Date(),
-        };
-      },
-    };
+    const email = 'donlotario@email.com';
+
+    const userRegisterReponse = await registerUseCase.execute({
+      name: 'Don Lotario',
+      email,
+      password: 'my-password',
+    });
+
+    expect(userRegisterReponse.user).toHaveProperty('id');
+    expect(userRegisterReponse.user.name).toBe('Don Lotario');
+    expect(userRegisterReponse.user.email).toBe(email);
+  });
+
+  it.skip('should not be able to create a new user with an existing email', async () => {
+    // TODO: implement this test
+  });
+
+  it('should hash the password', async () => {
+    const usersRepository = new InMemoryUsersRepository();
 
     const registerUseCase = new RegisterUseCase(usersRepository);
 
