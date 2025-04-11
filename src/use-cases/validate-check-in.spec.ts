87ada3ff17feb3048c3473c140e13b9ba65ab2b8
Fernando Santos <fernandosantoshs@gmail.com>
2025-04-11 17:54:58 -0300

    Adds test to prevent validate invalid check-in

--- src/use-cases/validate-check-in.spec.ts
+++ src/use-cases/validate-check-in.spec.ts
@@ -1,6 +1,7 @@
 import { InMemoryCheckInsRespository } from '@/repositories/in-memory/in-memory-check-ins-repository';
 import { beforeEach, describe, expect, it } from 'vitest';
 import { ValidateCheckInUseCase } from './validate-check-in';
+import { ResourceNotFoundError } from './errors/resource-not-found-error';
 
 let checkInsRepository: InMemoryCheckInsRespository;
 let sut: ValidateCheckInUseCase;
@@ -25,5 +26,11 @@ describe('Validate Check in Use Case', () => {
         validated_at: expect.any(Date),
       })
     );
+  });
+
+  it('should not be able to validate inexistent check-in', async () => {
+    await expect(async () => {
+      return sut.execute({ id: 'invalid-id' });
+    }).rejects.toBeInstanceOf(ResourceNotFoundError);
   });
 });
