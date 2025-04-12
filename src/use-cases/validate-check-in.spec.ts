4fac9f82585f464976642763601608e6b339b8aa
Fernando Santos <fernandosantoshs@gmail.com>
2025-04-12 18:54:48 -0300

    Adds test to prevent check-in after 20 minutes(max valid interval). Throwing error because the rule wasnt implemented yet

--- src/use-cases/validate-check-in.spec.ts
+++ src/use-cases/validate-check-in.spec.ts
@@ -1,5 +1,5 @@
 import { InMemoryCheckInsRespository } from '@/repositories/in-memory/in-memory-check-ins-repository';
-import { beforeEach, describe, expect, it } from 'vitest';
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
 import { ValidateCheckInUseCase } from './validate-check-in';
 import { ResourceNotFoundError } from './errors/resource-not-found-error';
 
@@ -10,6 +10,12 @@ describe('Validate Check in Use Case', () => {
   beforeEach(() => {
     checkInsRepository = new InMemoryCheckInsRespository();
     sut = new ValidateCheckInUseCase(checkInsRepository);
+
+    vi.useFakeTimers();
+  });
+
+  afterEach(() => {
+    vi.useRealTimers();
   });
 
   it('should be able to validate check-in', async () => {
@@ -32,5 +38,21 @@ describe('Validate Check in Use Case', () => {
     await expect(async () => {
       return sut.execute({ id: 'invalid-id' });
     }).rejects.toBeInstanceOf(ResourceNotFoundError);
+  });
+
+  it('should not be able to validate check-in after max valid interval (20min)', async () => {
+    vi.setSystemTime(new Date(2025, 0, 1, 8, 0, 0));
+
+    const checkIn = await checkInsRepository.create({
+      user_id: 'user-01',
+      gym_id: 'gym-01',
+      created_at: new Date(),
+    });
+
+    vi.setSystemTime(new Date(2025, 0, 1, 8, 22, 0));
+
+    await expect(async () => {
+      return await sut.execute({ id: checkIn.id });
+    }).rejects.toBeInstanceOf(Error);
   });
 });
