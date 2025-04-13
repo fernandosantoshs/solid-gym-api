fb1981f7290253d40b4737e37a2a7ea2a49f667b
Fernando Santos <fernandosantoshs@gmail.com>
2025-04-13 19:03:37 -0300

    Adds twenty minutes check-in validation in validate check-in use case

--- src/use-cases/validate-check-in.ts
+++ src/use-cases/validate-check-in.ts
@@ -1,6 +1,7 @@
 import { CheckInsRepository } from '@/repositories/check-ins-repository';
 import { CheckIn } from '@prisma/client';
 import { ResourceNotFoundError } from './errors/resource-not-found-error';
+import dayjs from 'dayjs';
 
 interface ValidateCheckInUseCaseRequest {
   id: string;
@@ -20,6 +21,15 @@ export class ValidateCheckInUseCase {
 
     if (!checkIn) {
       throw new ResourceNotFoundError();
+    }
+
+    const diffBetweenCheckInAndValidation = dayjs(new Date()).diff(
+      dayjs(checkIn.created_at),
+      'minutes'
+    );
+
+    if (diffBetweenCheckInAndValidation > 20) {
+      throw new Error();
     }
 
     checkIn.validated_at = new Date();
