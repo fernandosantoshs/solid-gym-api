314a637ba697aee5da6363d2bf0e0180006d6a89
Fernando Santos <fernandosantoshs@gmail.com>
2025-04-18 20:17:58 -0300

    Adds lib fastify/jwt; Adds app.register jwt and jwt_secret; Adds env variable jwt secret

--- src/env/index.ts
+++ src/env/index.ts
@@ -10,6 +10,7 @@ if (process.env.NODE_ENV == 'test') {
 const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'production', 'test']).default('dev'),
   PORT: z.coerce.number().default(3333),
+  JWT_SECRET: z.string(),
 });
 
 const envParse = envSchema.safeParse(process.env);
