248da730e9179ef671e847ddf2a2c5d01f6c1751
Fernando Santos <fernandosantoshs@gmail.com>
2025-05-08 11:00:41 -0300

    Rearranged user controllers to separated folder, so it wont be mixed with other routes

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -1,10 +1,10 @@
 import { FastifyInstance } from 'fastify';
-import { register } from './controllers/register';
-import { authentication } from './controllers/authentication';
-import { profile } from './controllers/profile';
+import { register } from './register';
+import { authentication } from './authentication';
+import { profile } from './profile';
 import { verifyJwt } from '@/middlewares/verify-jwt';
 
-export async function appRoutes(app: FastifyInstance) {
+export async function userRoutes(app: FastifyInstance) {
   app.post('/users', register);
   app.post('/sessions', authentication);
 
