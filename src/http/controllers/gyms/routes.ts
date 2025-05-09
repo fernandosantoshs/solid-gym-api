d70bf81cf573f3eeddef99ea5aa4838ecca5f09f
Fernando Santos <fernandosantoshs@gmail.com>
2025-05-09 20:20:24 -0300

    Adds search gyms controller and route

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,9 +1,12 @@
 import { app } from '@/app';
 import { create } from './create';
 import { verifyJwt } from '@/middlewares/verify-jwt';
+import { search } from './search';
 
 export async function gymRoutes() {
   app.addHook('onRequest', verifyJwt);
+
+  app.get('/gyms/search', search);
 
   app.post('/gyms', create);
 }
---
+++ src/http/controllers/gyms/search.ts
@@ -0,0 +1,18 @@
+import { makeSearchGymsUseCase } from '@/factories/make-search-gyms-use-case';
+import { FastifyReply, FastifyRequest } from 'fastify';
+import { z } from 'zod';
+
+export async function search(request: FastifyRequest, reply: FastifyReply) {
+  const searchGymsQueryParams = z.object({
+    query: z.string(),
+    page: z.coerce.number().min(1).default(1),
+  });
+
+  const { query, page } = searchGymsQueryParams.parse(request.params);
+
+  const searchGymsUseCase = makeSearchGymsUseCase();
+
+  const gyms = await searchGymsUseCase.execute({ query, page });
+
+  return reply.status(200).send({ gyms });
+}
