28f2ccab65bc607f81873c6ca803c19865badf1d
Fernando Santos <fernandosantoshs@gmail.com>
2025-05-26 11:44:29 -0300

    Adds refresh token controller and route

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -5,10 +5,13 @@ import { verifyJwt } from '@/middlewares/verify-jwt';
 import { register } from './register';
 import { authentication } from './authentication';
 import { profile } from './profile';
+import { refresh } from './refresh';
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register);
   app.post('/sessions', authentication);
+
+  app.patch('/token/refresh', refresh);
 
   /* Authenticated routes */
   app.get('/me', { onRequest: [verifyJwt] }, profile);
