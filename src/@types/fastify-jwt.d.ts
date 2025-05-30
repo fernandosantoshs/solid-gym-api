1d9e475cc6ea0c2e25dcd1fdb9c41ee63ac5f918
Fernando Santos <fernandosantoshs@gmail.com>
2025-05-29 21:46:47 -0300

    Adds user role in token and refreshToken; adds role in fastify-jwt interface

--- src/@types/fastify-jwt.d.ts
+++ src/@types/fastify-jwt.d.ts
@@ -4,6 +4,7 @@ declare module '@fastify/jwt' {
   export interface FastifyJWT {
     user: {
       sub: string;
+      role: string;
     }; // user type is return type of `request.user` object
   }
 }
